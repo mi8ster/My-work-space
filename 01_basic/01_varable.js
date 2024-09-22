74a336a818ede0ed212c42425f6a7fea1819c463
mi8ster <168485340+mi8ster@users.noreply.github.com>
2024-09-22 11:11:18 +0000

    varablework

---
+++ 01_basic/01_varable.js
@@ -0,0 +1,7 @@
+const accountId=1234
+let accountEmail="zeeshasn@gmail.com"
+var account_pass="malik123"
+
+accountEmail="malik@gmail.com"
+//console.log(accountEmail);
+console.table([accountEmail,accountId,account_pass])
