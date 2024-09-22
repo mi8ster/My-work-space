b5684ce7df1f743ca8ed7294171395554a3225e1
mi8ster <168485340+mi8ster@users.noreply.github.com>
2024-09-22 11:59:16 +0000

    arry concat

---
+++ 01_basic/01_arry.js
@@ -0,0 +1,7 @@
+const my_heros=["salmankhan","hamyusaeed"]
+const more_popular=["ajadav","kotle","Brusly"]
+
+//my_heros.push(more_popular)
+
+const all_hero= my_heros.concat(more_popular)
+console.log(all_hero);
