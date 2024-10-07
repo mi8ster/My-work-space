51f24228a5ba988afd3efb53bf99a9159bacbcb1
mi8ster <168485340+mi8ster@users.noreply.github.com>
2024-10-07 23:53:34 +0000

    javascrypt return  function

---
+++ 01_basic/01_function.js
@@ -0,0 +1,9 @@
+function addTwoNumber(number1 ,number2){
+    // console.log (number1 + number2);
+    // let result=number1+number2
+    // return result
+    return number1+number2
+}
+
+const result=addTwoNumber(8,6)
+console.log("Result: ",result);
